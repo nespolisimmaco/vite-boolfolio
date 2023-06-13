06f927fbd63d624605a094edd1dbf63f3106ff0e
nespolisimmaco <snespoli@yahoo.com>
2023-06-13 18:30:32 +0200

    Add: make a call to the API and take all the projects

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      projects: [
 
+      ]
+    }
+  },
+  mounted() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://localhost:8000/api/projects').then(resp => {
+        console.log(resp);
+        this.projects = resp.data.result;
+      })
+    }
+  }
+}
+</script>
+  
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Hello</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
