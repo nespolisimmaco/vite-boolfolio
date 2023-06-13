087822c3b5590766b599d0984de99365193386b9
nespolisimmaco <snespoli@yahoo.com>
2023-06-13 19:16:54 +0200

    Add: created projects' cards with ProjectCard component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 export default {
+  components: { ProjectCard },
   data() {
     return {
-      projects: [
-
-      ]
-    }
+      projects: []
+    };
   },
   mounted() {
     this.getProjects();
   },
   methods: {
     getProjects() {
-      axios.get('http://localhost:8000/api/projects').then(resp => {
-        console.log(resp);
+      axios.get("http://localhost:8000/api/projects").then(resp => {
         this.projects = resp.data.result;
-      })
+      });
     }
-  }
+  },
 }
 </script>
   
 <template>
   <div>
-    <h1>Hello</h1>
+    <h1>Lista dei progetti</h1>
+    <div class="row row-cols-4 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div class="card h-100">
+            <img v-if="project.image" :src="`http://localhost:8000/storage/${project.image}`" class="card-img-top"
+                :alt="project.title">
+            <div v-else>Nessuna immagine</div>
+            <div class="card-body text-center">
+                <h5>{{ project.title }}</h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
