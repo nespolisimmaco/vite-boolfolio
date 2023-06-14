1ca0e994fba87b00f3b5b682de4f65436cf6e836
nespolisimmaco <snespoli@yahoo.com>
2023-06-14 20:18:14 +0200

    Added pagination to projects' page

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -8,14 +8,13 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div class="card h-100">
-            <img v-if="project.image" :src="`http://localhost:8000/storage/${project.image}`" class="card-img-top"
-                :alt="project.title">
-            <div v-else>Nessuna immagine</div>
-            <div class="card-body text-center">
-                <h5>{{ project.title }}</h5>
-            </div>
+    <div class="card h-100">
+        <img v-if="project.image" :src="`http://localhost:8000/storage/${project.image}`" class="card-img-top"
+            :alt="project.title">
+        <div v-else>Nessuna immagine</div>
+        <div class="card-body text-center">
+            <h5>{{ project.title }}</h5>
+            <p>{{ project.description }}</p>
         </div>
     </div>
 </template>
